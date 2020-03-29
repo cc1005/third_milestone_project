6246ee67eea1dc952382a27b36e83784b33ca819
Conor Campbell <conorkampbell@hotmail.com>
2020-03-29 19:41:44 +0000

    More recent version of app.py left off previous commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-from flask_mail import Mail, Message
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
