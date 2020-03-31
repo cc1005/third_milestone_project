160872175b5c4b838f6f6651b8956c57463fb4d1
Conor Campbell <conorkampbell@hotmail.com>
2020-03-31 17:35:14 +0000

    Beginning to put together user/authentication functionality

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from flask_login import LoginManager
 from os import path
 if path.exists("env.py"):
     import env
@@ -11,19 +12,16 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'listOfPosts'
 app.config["MONGO_URI"] = os.environ.get("Mongoinfo")
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.environ['emailaddress'],
-    "MAIL_PASSWORD": os.environ['emailpassword']
-}
-
-app.config.update(mail_settings)
-mail = Mail(app)
+app.secret_key = os.environ.get("Serverkey")
 
 mongo = PyMongo(app)
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 @app.route('/')
 @app.route('/get_posts')
