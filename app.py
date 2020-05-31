aa61bff60374e4e143a2b434668a8c02c9159947
Conor Campbell <conorkampbell@hotmail.com>
2020-05-31 22:32:26 +0000

    template documents formatted appropriately to ensure readability

--- app.py
+++ app.py
@@ -14,18 +14,22 @@ app.config["MONGO_URI"] = os.environ.get("Mongoinfo")
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/get_posts')
 def get_posts():
     return render_template("posts.html", posts=mongo.db.adminPosts.find())
 
+
 @app.route('/add_post')
 def add_post():
     return render_template('addpost.html')
 
+
 @app.route('/approve_posts')
 def update_delete():
     return render_template("approveposts.html", posts=mongo.db.adminPosts.find())
+
 
 @app.route('/insert_post', methods=['POST'])
 def insert_post():
@@ -33,40 +37,45 @@ def insert_post():
     posts.insert_one(request.form.to_dict())
     return redirect(url_for('get_posts'))
 
+
 @app.route('/edit_post/<post_id>')
 def edit_post(post_id):
     the_post = mongo.db.adminPosts.find_one({"_id": ObjectId(post_id)})
     return render_template('editpost.html', post=the_post)
 
+
 @app.route('/update_post/<post_id>', methods=["POST"])
 def update_post(post_id):
     posts = mongo.db.adminPosts
-    posts.update( {'_id': ObjectId(post_id)},
-    {
-        'title':request.form.get('title'),
-        'dates':request.form.get('dates'),
-        'article':request.form.get('article'),
+    posts.update({'_id': ObjectId(post_id)},
+                 {
+        'title': request.form.get('title'),
+        'dates': request.form.get('dates'),
+        'article': request.form.get('article'),
         'author': request.form.get('author'),
     })
     return redirect(url_for('get_posts'))
 
+
 @app.route('/approve_post/<post_id>', methods=["POST"])
 def approve_post(post_id):
     posts = mongo.db.adminPosts
-    posts.update( {'_id': ObjectId(post_id)},
-    {
-        'approved':request.form.get('approved'),
-        'title':request.form.get('title'),
-        'dates':request.form.get('dates'),
-        'article':request.form.get('article'),
+    posts.update({'_id': ObjectId(post_id)},
+                 {
+        'approved': request.form.get('approved'),
+        'title': request.form.get('title'),
+        'dates': request.form.get('dates'),
+        'article': request.form.get('article'),
         'author': request.form.get('author'),
     })
     return redirect(url_for('get_posts'))
+
 
 @app.route('/delete_post/<post_id>')
 def delete_post(post_id):
     mongo.db.adminPosts.remove({'_id': ObjectId(post_id)})
     return redirect(url_for('get_posts'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
